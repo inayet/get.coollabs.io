5a8844a917109bdee8208bd6437ca91390e3c89f
Andras Bacsai <andras.bacsai@gmail.com>
2021-04-10 22:27:30 +0200

    listen on all

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ fastify.get('/', function (req, reply) {
 
 const start = async () => {
   try {
-    await fastify.listen(3000)
+    await fastify.listen('0.0.0.0', 3000)
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
