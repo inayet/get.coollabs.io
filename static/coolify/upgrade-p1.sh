5a79f25518b202276e7f3e07059a5f39f308ac71
Andras Bacsai <andras.bacsai@gmail.com>
2021-04-10 23:22:38 +0200

    it works

--- static/coolify/upgrade-p1.sh
+++ static/coolify/upgrade-p1.sh
@@ -5,7 +5,6 @@ if [ $WHO != 'root' ]; then
     exit
 fi
 
-echo "Hello update"
 GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git pull
 docker build --label coolify-reserve=true -t coolify-base -f /usr/src/app/install/Dockerfile-base .
 docker run --rm -w /usr/src/app coolify-base node /usr/src/app/install/check.js
