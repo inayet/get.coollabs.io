a6cb701d9d380c32902af162275b658c006a8bbe
Andras Bacsai <andras.bacsai@gmail.com>
2021-04-14 10:18:19 +0200

    Upgrade script p1

--- static/coolify/upgrade-p1.sh
+++ static/coolify/upgrade-p1.sh
@@ -5,11 +5,11 @@ if [ $WHO != 'root' ]; then
     echo 'You are not root. Ooops!'
     exit
 fi
-
+# Dockerfile-base-new should be renamed after months
 GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git pull
 docker build --label coolify-reserve=true -t coolify-binaries -f install/Dockerfile-binaries .
 docker build --label coolify-reserve=true -t coolify-base-nodejs -f install/Dockerfile-base-nodejs .
-docker build --label coolify-reserve=true -t coolify-base -f install/Dockerfile-base .
+docker build --label coolify-reserve=true -t coolify-base -f install/Dockerfile-base-new .
 docker run --rm -w /usr/src/app coolify-base node /usr/src/app/install/check.js
 
 set -a && source .env && set +a
