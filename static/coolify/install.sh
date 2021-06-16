5fbd6492d5b1419d62a44f6470ff7229dae10953
Andras Bacsai <andras.bacsai@gmail.com>
2021-06-16 23:29:32 +0200

    New installation script! :tada:

--- static/coolify/install.sh
+++ static/coolify/install.sh
@@ -1,35 +1,11 @@
 #!/bin/bash
-echo -e 'Welcome to coolLabs installer.\n'
+echo -e "####################################\nIf you interested in what this script does, go to https://github.com/coollabsio/get.coollabs.io\n####################################\n\n"
 WHO=$(whoami)
 if [ $WHO != 'root' ]; then
     echo 'Run as root please: sudo /bin/bash -c "$(curl -fsSL https://get.coollabs.io/coolify/install.sh)"'
     exit
 fi
-
-PS3='Which application you would like to install: '
-options=("coolify" "coolify-debug" "exit")
-
-select opt in "${options[@]}"
-do
-    case $opt in
-        "coolify")
-            rm ./coolify-installer
-            curl "https://storage.coollabs.io/coolify-installer" -o coolify-installer
-            chmod +x "./coolify-installer"
-            ./coolify-installer
-            break
-            ;;
-        "coolify-debug")
-            rm ./coolify-installer
-            curl "https://storage.coollabs.io/coolify-installer" -o coolify-installer
-            chmod +x "./coolify-installer"
-            ./coolify-installer -d
-            break
-            ;;
-        "exit")
-            break
-            ;;
-        *)  echo "Okay, nice try."
-            break
-    esac
-done
+mkdir -p /data/coolify
+mkdir -p /root/coollabs/coolify
+/bin/bash -c "$(curl -fsSL https://get.docker.com)"
+docker run --pull always --rm  -v "/var/run/docker.sock:/var/run/docker.sock" -v "/data/coolify:/data/coolify" -v "/root/coollabs/coolify:/usr/src/app/coollabs/coolify" -p "8080:8080" --name coollabs-installer coollabsio/coollabs-installer
