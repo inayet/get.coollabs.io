8b3c868d41f315237accdb6755315fb22427f24a
Andras Bacsai <andras.bacsai@gmail.com>
2022-04-20 14:11:53 +0200

    Update install.sh

--- static/coolify/install.sh
+++ static/coolify/install.sh
@@ -3,30 +3,30 @@
 [ ! -n "$BASH_VERSION" ] && echo "You can only run this script with bash, not sh / dash." && exit 1
 
 set -eou pipefail
-VERSION="v0.1.0"
-
+VERSION="v1.0.0"
 ARCH=$(uname -m)
-
 WHO=$(whoami)
-
-COOLIFY_APP_ID=$(cat /proc/sys/kernel/random/uuid)
-COOLIFY_SECRET_KEY=$(echo $(($(date +%s%N) / 1000000)) | sha256sum | base64 | head -c 32)
-COOLIFY_SENTRY_DSN="https://9e7a74326f29422584d2d0bebdc8b7d3@o1082494.ingest.sentry.io/6091062"
-COOLIFY_WHITE_LABELED=false
+DEBUG=0
+FORCE=0
 
 DOCKER_MAJOR=20
 DOCKER_MINOR=10
 DOCKER_VERSION_OK="nok"
 
-FORCE=0
-
+COOLIFY_APP_ID=$(cat /proc/sys/kernel/random/uuid)
+COOLIFY_SECRET_KEY=$(echo $(($(date +%s%N) / 1000000)) | sha256sum | base64 | head -c 32)
+COOLIFY_SENTRY_DSN="https://9e7a74326f29422584d2d0bebdc8b7d3@o1082494.ingest.sentry.io/6091062"
+COOLIFY_WHITE_LABELED=false
+COOLIFY_WHITE_LABELED_ICON=
 COOLIFY_CONF_FOUND=$(find ~ -path '*/coolify/.env')
+
 if [ -n "$COOLIFY_CONF_FOUND" ]; then
     eval "$(grep ^COOLIFY_APP_ID= $COOLIFY_CONF_FOUND)"
     eval "$(grep ^COOLIFY_SECRET_KEY= $COOLIFY_CONF_FOUND)"
     eval "$(grep ^COOLIFY_DATABASE_URL= $COOLIFY_CONF_FOUND)"
     eval "$(grep ^COOLIFY_SENTRY_DSN= $COOLIFY_CONF_FOUND)"
     eval "$(grep ^COOLIFY_HOSTED_ON= $COOLIFY_CONF_FOUND)"
+    eval "$(grep ^COOLIFY_WHITE_LABELED_ICON= $COOLIFY_CONF_FOUND)"
 else
     COOLIFY_CONF_FOUND=${COOLIFY_CONF_FOUND:="$HOME/coolify/.env"}
 fi
@@ -36,12 +36,6 @@ if [ ! -d ~/coolify ]; then
     mkdir ~/coolify
 fi
 
-function doNotTrack() {
-      DO_NOT_TRACK=1
-      COOLIFY_SENTRY_DSN=
-      COOLIFY_APP_ID=
-}
-
 if [ -z ${DO_NOT_TRACK+0} ]; then
     DO_NOT_TRACK=0
 else 
@@ -50,59 +44,65 @@ else
     fi
 fi
 
-POSITIONAL_ARGS=()
-
-while [[ $# -gt 0 ]]; do
-  case $1 in
-    -h|--help)
-    echo -e "Coolify installer $VERSION
-(source code: https://github.com/coollabsio/get.coollabs.io/blob/main/static/coolify/install.sh)\n
-Usage: install.sh [options...] 
-    -d, --debug         Show debug logs during installation.
-    -f, --force         Force installation, no questions asked.
-    --do-not-track      Opt-out of telemetry. You can set export DO_NOT_TRACK=1 in advance.
-    --white-labeled     Install white labeled version. Contact me before using it (https://docs.coollabs.io/contact)".
-    exit 1
-    ;;
-    -d|--debug)
-      set -x
-      shift
-      ;;
-    -f|--force)
-      FORCE=1
-      shift
-      ;;
-    --do-not-track)
-      doNotTrack
-      shift
-      ;;
-    --white-labeled)
-      COOLIFY_WHITE_LABELED="true"
-      shift
-      ;;
-    -*|--*)
-      echo "Unknown option $1"
-      exit 1
-      ;;
-    *)
-      POSITIONAL_ARGS+=("$1")
-      shift
-      ;;
-  esac
-done
-
-set -- "${POSITIONAL_ARGS[@]}"
-
-function errorchecker() {
+die() { echo "$*" >&2; exit 2; }
+needs_arg() { if [ -z "$OPTARG" ]; then die "No arg for --$OPT option"; fi; }
+errorchecker() {
     exitCode=$?
     if [ $exitCode -ne "0" ]; then
         echo "$0 exited unexpectedly with status: $exitCode"
         exit $exitCode
     fi
 }
+doNotTrack() {
+      DO_NOT_TRACK=1
+      COOLIFY_SENTRY_DSN=
+      COOLIFY_APP_ID=
+}
+
+while getopts hdfnwi:-: OPT; do
+  if [ "$OPT" = "-" ]; then
+    OPT="${OPTARG%%=*}"
+    OPTARG="${OPTARG#$OPT}" 
+    OPTARG="${OPTARG#=}"
+  fi
+  case "$OPT" in
+    h | help ) 
+        echo -e "Coolify installer $VERSION
+(source code: https://github.com/coollabsio/get.coollabs.io/blob/main/static/coolify/install.sh)\n
+Usage: install.sh [options...] 
+    -h, --help                  Show this help menu.
+    -d, --debug                 Show debug logs during installation.
+    -f, --force                 Force installation, no questions asked.\n
+    -n, --do-not-track          Opt-out of telemetry. You can set export DO_NOT_TRACK=1 in advance.\n
+    -w, --white-labeled         Install white-labeled version. Contact me before using it (https://docs.coollabs.io/contact)
+    -i, --white-labeled-logo    Add your remote logo for your white-labeled version. Should be a http/https URL."
+    exit 1;
+    ;;
+    d | debug )                 DEBUG=1; set -x;;
+    f | force )                 FORCE=1;;
+    n | do-not-track )          doNotTrack;;
+    w | white-labeled )         COOLIFY_WHITE_LABELED="true";;
+    i | white-labeled-logo )    needs_arg; COOLIFY_WHITE_LABELED_ICON="$OPTARG"; COOLIFY_WHITE_LABELED="true";;
+    ??* )                       die "Illegal option --$OPT";;
+    ? )                         exit 2 ;;
+  esac
+done
+
+shift $((OPTIND-1))
 trap 'errorchecker' EXIT
+
+if [ $DEBUG -eq 1 ]; then
+    echo "FORCE=$FORCE" 
+    echo "COOLIFY_APP_ID=$COOLIFY_APP_ID"
+    echo "COOLIFY_SECRET_KEY=$COOLIFY_SECRET_KEY"
+    echo "COOLIFY_DATABASE_URL=$COOLIFY_DATABASE_URL"
+    echo "COOLIFY_SENTRY_DSN=$COOLIFY_SENTRY_DSN"
+    echo "COOLIFY_HOSTED_ON=$COOLIFY_HOSTED_ON"
+    echo "COOLIFY_WHITE_LABELED=$COOLIFY_WHITE_LABELED"
+    echo "COOLIFY_WHITE_LABELED_ICON=$COOLIFY_WHITE_LABELED_ICON" 
+fi
 if [ $FORCE -eq 1 ]; then
-    echo "Installing Coolify with force option."
+    echo -e "Installing Coolify with force option!"
 else
     echo -e "Welcome to Coolify installer!"
     echo -e "This script will install all requirements to run Coolify."
@@ -121,13 +121,13 @@ if [ $WHO != 'root' ]; then
     exit 1
 fi
 
-function restartDocker() {
+restartDocker() {
    # Restarting docker daemon
    sudo systemctl daemon-reload
    sudo systemctl restart docker
 }
 
-function dockerConfiguration() {
+dockerConfiguration() {
     cat <<EOF >/etc/docker/daemon.json
 {
     "log-driver": "json-file",
@@ -152,13 +152,14 @@ function dockerConfiguration() {
 }
 EOF
 }
-function saveCoolifyConfiguration() {
+saveCoolifyConfiguration() {
       echo "COOLIFY_APP_ID=$COOLIFY_APP_ID
 COOLIFY_SECRET_KEY=$COOLIFY_SECRET_KEY
 COOLIFY_DATABASE_URL=file:../db/prod.db
 COOLIFY_SENTRY_DSN=$COOLIFY_SENTRY_DSN
 COOLIFY_HOSTED_ON=docker
-COOLIFY_WHITE_LABELED=$COOLIFY_WHITE_LABELED" > $COOLIFY_CONF_FOUND
+COOLIFY_WHITE_LABELED=$COOLIFY_WHITE_LABELED 
+COOLIFY_WHITE_LABELED_ICON=$COOLIFY_WHITE_LABELED_ICON"> $COOLIFY_CONF_FOUND
 }
 # Check docker version
 if [ ! -x "$(command -v docker)" ]; then
@@ -295,9 +296,11 @@ fi
 if [ $FORCE -ne 1 ]; then
     echo "Installing Coolify."
 fi
+echo "Pulling Coolify latest image."
 sudo docker pull -q coollabsio/coolify:latest > /dev/null
+echo "Starting Coolify."
 cd ~/coolify && sudo docker run -tid --env-file $COOLIFY_CONF_FOUND -v /var/run/docker.sock:/var/run/docker.sock -v coolify-db-sqlite coollabsio/coolify:latest /bin/sh -c "env | grep COOLIFY > .env && docker compose up -d --force-recreate" > /dev/null
 
-echo -e "\nCongratulations! Your Coolify instance is ready to use."
+echo -e "Congratulations! Your Coolify instance is ready to use.\n"
 echo "Please visit http://$(curl -4s https://ifconfig.io):3000 to get started."
 echo "It will take a few minutes to start up, don't worry."
