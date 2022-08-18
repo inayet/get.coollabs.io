75a950017e86cd6148535c8173cc547958f5d0bd
Andras Bacsai <andras.bacsai@gmail.com>
2022-08-18 10:01:45 +0200

    Update install.sh

--- static/coolify/install.sh
+++ static/coolify/install.sh
@@ -3,7 +3,7 @@
 [ ! -n "$BASH_VERSION" ] && echo "You can only run this script with bash, not sh / dash." && exit 1
 
 set -eou pipefail
-VERSION="v1.3.0"
+VERSION="v1.4.0"
 ARCH=$(uname -m)
 WHO=$(whoami)
 DEBUG=0
@@ -62,7 +62,13 @@ doNotTrack() {
       COOLIFY_APP_ID=
 }
 
-while getopts hvdfnawi:x:-: OPT; do
+restartCoolify() {
+    echo "Restarting Coolify."
+    cd ~/coolify && sudo docker run --rm -tid --env-file $COOLIFY_CONF_FOUND -v /var/run/docker.sock:/var/run/docker.sock -v coolify-db-sqlite coollabsio/coolify:$VERSION /bin/sh -c "env | grep COOLIFY > .env && docker compose up -d --force-recreate" > /dev/null
+    exit 0
+}
+
+while getopts hvdfrnawi:x:-: OPT; do
   if [ "$OPT" = "-" ]; then
     OPT="${OPTARG%%=*}"
     OPTARG="${OPTARG#$OPT}" 
@@ -75,18 +81,25 @@ while getopts hvdfnawi:x:-: OPT; do
 Usage: install.sh [options...] 
     -h, --help                  Show this help menu.
     -v, --version               Show script version.
+
     -d, --debug                 Show debug logs during installation.
     -f, --force                 Force installation, no questions asked.\n
+
+    -r, --restart               Only restart Coolify. Nothing more.\n
+
     -n, --do-not-track          Opt-out of telemetry. You can set export DO_NOT_TRACK=1 in advance.\n
     -a, --auto-update           Warning: TESTING PHASE, DO NOT USE IT YET! Enable auto update feature of Coolify. \n
+
     -w, --white-labeled         Install white-labeled version. Contact me before using it (https://docs.coollabs.io/contact)
     -i, --white-labeled-logo    Add your remote logo for your white-labeled version. Should be a http/https URL.
+
     -x, --force-version         Force installation of a specific version of Coolify."
     exit 1;
     ;;
     d | debug )                 DEBUG=1; set -x;;
     f | force )                 FORCE=1;;
     v | version )               echo "$VERSION" && exit 1;;
+    r | restart )               restartCoolify;;
     n | do-not-track )          doNotTrack;;
     a | auto-update )           COOLIFY_AUTO_UPDATE="true";;
     w | white-labeled )         COOLIFY_WHITE_LABELED="true";;
@@ -104,9 +117,9 @@ if [ $DEBUG -eq 1 ]; then
     echo "FORCE=$FORCE" 
     echo "COOLIFY_APP_ID=$COOLIFY_APP_ID"
     echo "COOLIFY_SECRET_KEY=$COOLIFY_SECRET_KEY"
-    echo "COOLIFY_DATABASE_URL=$COOLIFY_DATABASE_URL"
+    echo "COOLIFY_DATABASE_URL=${COOLIFY_DATABASE_URL:-../db/prod.db}"
     echo "COOLIFY_SENTRY_DSN=$COOLIFY_SENTRY_DSN"
-    echo "COOLIFY_HOSTED_ON=$COOLIFY_HOSTED_ON"
+    echo "COOLIFY_HOSTED_ON=${COOLIFY_HOSTED_ON:-docker}"
     echo "COOLIFY_WHITE_LABELED=$COOLIFY_WHITE_LABELED"
     echo "COOLIFY_WHITE_LABELED_ICON=$COOLIFY_WHITE_LABELED_ICON" 
     echo "COOLIFY_AUTO_UPDATE=$COOLIFY_AUTO_UPDATE"
@@ -131,6 +144,7 @@ if [ $WHO != 'root' ]; then
     exit 1
 fi
 
+
 restartDocker() {
    # Restarting docker daemon
    sudo systemctl daemon-reload
@@ -138,7 +152,8 @@ restartDocker() {
 }
 
 dockerConfiguration() {
-    cat <<EOF >/etc/docker/daemon.json
+    mkdir -p /etc/docker/ /var/snap/docker/current/config/
+    cat <<EOF | tee /etc/docker/daemon.json  /var/snap/docker/current/config/daemon.json > /dev/null
 {
     "log-driver": "json-file",
     "log-opts": {
@@ -218,7 +233,7 @@ if [ $DOCKER_VERSION_OK == 'nok' ]; then
     exit 1
 fi
 
-if [ -f "/etc/docker/daemon.json" ]; then
+if [ -f "/etc/docker/daemon.json" ] || [ -f "/var/snap/docker/current/config/daemon.json" ]; then
     if [ $FORCE -eq 1 ]; then
         # Adding docker daemon configuration
         echo 'Configuring Docker daemon.'
@@ -242,7 +257,8 @@ if [ -f "/etc/docker/daemon.json" ]; then
     fi
 else
     # Adding docker daemon configuration
-    cat <<EOF >/etc/docker/daemon.json
+    mkdir -p /etc/docker/ /var/snap/docker/current/config/
+    cat <<EOF | tee /etc/docker/daemon.json  /var/snap/docker/current/config/daemon.json > /dev/null
 {
     "log-driver": "json-file",
     "log-opts": {
@@ -276,10 +292,10 @@ if [ ! -x ~/.docker/cli-plugins/docker-compose ]; then
         sudo mkdir -p ~/.docker/cli-plugins/
     fi
     if [ ARCH == 'arm64' ] || [ ARCH == 'aarch64' ]; then
-        sudo curl --silent -SL https://cdn.coollabs.io/bin/linux/arm64/docker-compose-linux-2.3.4 -o ~/.docker/cli-plugins/docker-compose
+        sudo curl --silent -SL https://cdn.coollabs.io/bin/linux/arm64/docker-compose-linux-2.6.1 -o ~/.docker/cli-plugins/docker-compose
         sudo chmod +x ~/.docker/cli-plugins/docker-compose
     else 
-        sudo curl --silent -SL https://cdn.coollabs.io/bin/linux/amd64/docker-compose-linux-2.3.4 -o ~/.docker/cli-plugins/docker-compose
+        sudo curl --silent -SL https://cdn.coollabs.io/bin/linux/amd64/docker-compose-linux-2.6.1 -o ~/.docker/cli-plugins/docker-compose
         sudo chmod +x ~/.docker/cli-plugins/docker-compose
     fi
 fi
@@ -289,7 +305,7 @@ if [ $FORCE -eq 1 ]; then
 else
     if [ -f "$COOLIFY_CONF_FOUND" ]; then
         while true; do
-                    read -p "Coolify configuration found (${COOLIFY_CONF_FOUND}). I will overwrite it, okay? [Yy/Nn] " yn
+                    read -p "Coolify configuration found (${COOLIFY_CONF_FOUND}). I will overwrite it, okay?  [Yy/Nn] " yn
                     case $yn in
                     [Yy]*)
                         saveCoolifyConfiguration
